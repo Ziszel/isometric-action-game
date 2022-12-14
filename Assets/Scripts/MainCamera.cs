fd2a85186fcbcf4f2ec7a1729cd639ea00532a04
ziszel <the8thnoteofficial@gmail.com>
2022-12-14 15:38:04 +0000

    Updated MainCamera.cs so camera can rotate through mouse input

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -3,22 +3,39 @@ using UnityEngine;
 public class MainCamera : MonoBehaviour
 {
     public Transform Player;
+    private Vector2 mousePosition;
+    private float pitch = 0.0f;
+    private float rotationSpeed = 4.0f;
+    private float _friction = 0.5f;
 
     private Vector3 _offset;
     // Start is called before the first frame update
     void Start()
     {
         // the difference between the camera and the player when the scene is loaded
-        _offset = transform.position - Player.transform.position;
+        SetOffset();
     }
-    
-    
 
     // LateUpdate() is called after Update() each frame
     // this ensures the player moves THEN the camera position is recalculated
     // https://www.maxester.com/blog/2020/02/24/how-do-you-make-the-camera-follow-the-player-in-unity-3d/
-    void LateUpdate()
+    private void LateUpdate()
     {
-        transform.position = Player.transform.position + _offset;
+        // Get the mouse in a similar way to buttons/keys and set the pitch relevant to the movement of the mouse
+        pitch -= rotationSpeed * Input.GetAxis("Mouse X");
+
+        // Update the camera position to track the player, and rotate around the player
+        transform.position = (Player.transform.position + _offset);
+        transform.RotateAround(Player.position, Vector3.up, pitch * Time.deltaTime);
+        
+        // A rotate requires that _offset be re-calculated
+        SetOffset();
+        
+        //pitch *= _friction;
+    }
+
+    private void SetOffset()
+    {
+        _offset = transform.position - Player.transform.position;
     }
 }
