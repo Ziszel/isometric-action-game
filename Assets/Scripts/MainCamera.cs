4ecf4159c7a6e10ac0c92cd83cad5791e55882e4
Ziszel <the8thnoteofficial@gmail.com>
2022-12-21 09:31:47 +0000

    Updated MainCamera.cs to edit FOV dependant on player speed

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class MainCamera : MonoBehaviour
 {
-    public Transform Player;
+    public Player Player;
+    public Camera mainCamera;
     private Vector2 mousePosition;
     private float pitch = 0.0f;
     private float rotationSpeed = 5.0f;
@@ -21,12 +22,17 @@ public class MainCamera : MonoBehaviour
     // https://www.maxester.com/blog/2020/02/24/how-do-you-make-the-camera-follow-the-player-in-unity-3d/
     private void LateUpdate()
     {
+        var playerVelocity = new Vector3(Player.rb.velocity.x, 0.0f, Player.rb.velocity.z).magnitude;
+        mainCamera.fieldOfView = 60 + playerVelocity;
         // Get the mouse in a similar way to buttons/keys and set the pitch relevant to the movement of the mouse
+        // Mouse sensitivity feels different in WebGL compared to editor, this is a long standing Unity bug:
+        // https://forum.unity.com/threads/mouse-sensitivity-in-webgl-way-too-sensitive.411574/#post-3421405
+        // NOT IMPLEMENTED CURRENTLY, Mouse slows too fast, feels bad. Need to look into this later.
         pitch += rotationSpeed * Input.GetAxis("Mouse X");
 
         // Update the camera position to track the player, and rotate around the player
         transform.position = (Player.transform.position + _offset);
-        transform.RotateAround(Player.position, Vector3.up, pitch * Time.deltaTime);
+        transform.RotateAround(Player.transform.position, Vector3.up, pitch * Time.deltaTime);
         
         // A rotate requires that _offset be re-calculated
         SetOffset();
